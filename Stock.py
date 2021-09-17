a72c6ec72af22b168b01f8a616a8ac088e29156b
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-09-17 19:51:53 +0530

    Miui Wall Added

--- Stock.py
+++ Stock.py
@@ -65,7 +65,8 @@ def addData(name):
     imageData["id"] = len(temp[name]['Images'])+1
     imageData["heroID"] = name+str(len(temp[name]['Images'])+1)
     imageData["downloads"] = 0
-    imageData["name"] = input("name : ")
+    # imageData["name"] = input("name : ")
+    imageData["name"] = 'MI MIX 4'
     imageData["designer"] = name
     # imageData["category"] = input("category : ")
     imageData["size"] = input("size : ")
