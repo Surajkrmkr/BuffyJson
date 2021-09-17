7e1b3fe8da19a3f0906a2ca26d6dc05fa6317fba
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-09-17 20:06:32 +0530

    Realme walls added

--- Stock.py
+++ Stock.py
@@ -66,7 +66,7 @@ def addData(name):
     imageData["heroID"] = name+str(len(temp[name]['Images'])+1)
     imageData["downloads"] = 0
     # imageData["name"] = input("name : ")
-    imageData["name"] = 'OnePlus Nord'
+    imageData["name"] = 'ColorOS 12'
     imageData["designer"] = name
     # imageData["category"] = input("category : ")
     imageData["size"] = input("size : ")
