d58a01268ace6a0c3a6b153d1120b50f9d814f28
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-09-17 20:16:57 +0530

    Samsung walls added

--- Stock.py
+++ Stock.py
@@ -66,7 +66,7 @@ def addData(name):
     imageData["heroID"] = name+str(len(temp[name]['Images'])+1)
     imageData["downloads"] = 0
     # imageData["name"] = input("name : ")
-    imageData["name"] = 'Vivo'
+    imageData["name"] = 'Galaxy Z Flip'
     imageData["designer"] = name
     # imageData["category"] = input("category : ")
     imageData["size"] = input("size : ")
