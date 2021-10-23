c677ff180e375eeb588b34aa6bd0bfc068fdcb9b
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-10-23 10:50:27 +0530

    abstract added

--- Add_auto_data.py
+++ Add_auto_data.py
@@ -10,8 +10,8 @@ path = "data/Category/"
 pickedWallspaths = os.listdir(rootPath)
 
 
-name = 'Animals'
-date = "Oct 19"
+name = 'Abstract'
+date = "Oct 23"
 
 # print(" 1 . Abstract      2 . Amoled     3 . Animals                 4 . Anime")
 #     print(" 5 . Cars & Bike   6 . Cartoon    7 . Flower                 8 . Games")
