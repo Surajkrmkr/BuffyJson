1aeb7d584da099a9cd69d14baf819a7f417bd33a
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-10-26 18:27:35 +0530

    amoled added

--- Add_auto_data.py
+++ Add_auto_data.py
@@ -10,8 +10,8 @@ path = "data/Category/"
 pickedWallspaths = os.listdir(rootPath)
 
 
-name = 'Abstract'
-date = "Oct 23"
+name = 'Amoled'
+date = "Oct 26"
 
 # print(" 1 . Abstract      2 . Amoled     3 . Animals                 4 . Anime")
 #     print(" 5 . Cars & Bike   6 . Cartoon    7 . Flower                 8 . Games")
