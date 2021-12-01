f089c3a4d332f1fccd944fdb5cbde5515be2d79d
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-12-01 12:15:29 +0530

    added

--- Add_auto_data.py
+++ Add_auto_data.py
@@ -10,8 +10,8 @@ path = "data/Category/"
 pickedWallspaths = os.listdir(rootPath)
 
 
-name = 'Minimal'
-date = "Nov 29"
+name = 'Suraj'
+date = "Dec 1"
 
 #     print(" 1 . Abstract      2 . Amoled     3 . Animals                4 . Anime")
 #     print(" 5 . Cars & Bike   6 . Cartoon    7 . Flower                 8 . Games")
