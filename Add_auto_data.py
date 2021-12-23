d0be500e9043c1ffd1dbe5b3e17757ec53b39c88
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-12-23 18:57:04 +0530

    New walls added

--- Add_auto_data.py
+++ Add_auto_data.py
@@ -10,8 +10,8 @@ path = "data/Category/"
 pickedWallspaths = os.listdir(rootPath)
 
 
-name = 'Suraj'
-date = "Dec 1"
+name = 'Super Heroes'
+date = "Dec 23"
 
 #     print(" 1 . Abstract      2 . Amoled     3 . Animals                4 . Anime")
 #     print(" 5 . Cars & Bike   6 . Cartoon    7 . Flower                 8 . Games")
